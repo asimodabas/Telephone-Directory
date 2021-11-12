388ef47aac1e0e498b155fef7046f682fe326f20
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-11-12 13:29:14 +0300

    Initial commit

---
+++ app/src/main/java/com/asimodabas/telephone_directory_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.asimodabas.telephone_directory_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
