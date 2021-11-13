ea6902868e9e68dc006a252347f720360c08e01c
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-11-13 03:04:40 +0300

    Adapter coding completed

---
+++ app/src/main/java/com/asimodabas/telephone_directory_app/Kisiler.kt
@@ -0,0 +1,6 @@
+package com.asimodabas.telephone_directory_app
+
+data class Kisiler(var kisi_id:Int,var kisi_ad:String,var kisi_tel:String){
+
+
+}
---
+++ app/src/main/java/com/asimodabas/telephone_directory_app/KisilerAdapter.kt
@@ -0,0 +1,45 @@
+package com.asimodabas.telephone_directory_app
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class KisilerAdapter(private val mContext: Context, private val kisilerListe: List<Kisiler>) :
+    RecyclerView.Adapter<KisilerAdapter.CardTasarimTutucu>() {
+
+    inner class CardTasarimTutucu(tasarim: View) : RecyclerView.ViewHolder(tasarim) {
+        var textViewKisiBilgi: TextView
+        var imageViewNokta: ImageView
+
+        init {
+            textViewKisiBilgi = tasarim.findViewById(R.id.textViewKisiBilgi)
+            imageViewNokta = tasarim.findViewById(R.id.imageViewNokta)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimTutucu {
+        val tasarim =
+            LayoutInflater.from(mContext).inflate(R.layout.kisi_card_tasarim, parent, false)
+        return CardTasarimTutucu(tasarim)
+    }
+
+    override fun onBindViewHolder(holder: CardTasarimTutucu, position: Int) {
+        val kisi = kisilerListe.get(position)
+
+        holder.textViewKisiBilgi.text="${kisi.kisi_ad}-${kisi.kisi_tel}"
+        holder.imageViewNokta.setOnClickListener {
+
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return kisilerListe.size
+    }
+
+
+}
